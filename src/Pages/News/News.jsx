5e5bed685f6b9b89046c18667a117c8f5239ca95
nidhi1912001 <nidhishah1912001@gmail.com>
2023-11-07 09:12:03 +0530

    carousel done

--- src/Pages/News/News.jsx
+++ src/Pages/News/News.jsx
@@ -11,147 +11,61 @@ import Container from "../../Component/Container/Container";
 
 const News = () => {
   const newsList = [ {
-    id: 1, icon: <SlCalender/>, date: "5th January,2023", detail: "Three Ways Leaders Should\n" +
+    id: 1, icon: <SlCalender/>, date: "5th January,2023", detail: "111Three Ways Leaders Should\n" +
       "Pursue Breakthrough Innovation\n" +
       "During...", img: `${news1}`
   },
     {
-      id: 2, icon: <SlCalender/>, date: "5th January,2023", detail: "Three Ways Leaders Should\n" +
+      id: 2, icon: <SlCalender/>, date: "5th January,2023", detail: "2222Three Ways Leaders Should\n" +
         "Pursue Breakthrough Innovation\n" +
         "During...", img: `${news2}`
     },
     {
-      id: 3, icon: <SlCalender/>, date: "5th January,2023", detail: "Three Ways Leaders Should\n" +
+      id: 3, icon: <SlCalender/>, date: "5th January,2023", detail: "333Three Ways Leaders Should\n" +
         "Pursue Breakthrough Innovation\n" +
         "During...", img: `${news3}`
     },
-    {id: 4, icon: <SlCalender/>, date: "5th January,2023", detail: "Three Ways Leaders Should\n" +
+    {id: 4, icon: <SlCalender/>, date: "5th January,2023", detail: "444Three Ways Leaders Should\n" +
         "Pursue Breakthrough Innovation11\n" +
         "During...", img: `${news3}`},
-    {id: 5, icon: <SlCalender/>, date: "5th January,2023", detail: "Three Ways Leaders Should\n" +
+    {id: 5, icon: <SlCalender/>, date: "5th January,2023", detail: "555Three Ways Leaders Should\n" +
         "Pursue Breakthrough Innovation11\n" +
         "During...", img: `${news3}`},
     {
-      id: 6, icon: <SlCalender/>, date: "5th January,2023", detail: "Three Ways Leaders Should\n" +
+      id: 6, icon: <SlCalender/>, date: "5th January,2023", detail: "666Three Ways Leaders Should\n" +
         "Pursue Breakthrough Innovation\n" +
         "During...", img: `${news2}`
     },
     {
-      id: 7, icon: <SlCalender/>, date: "5th January,2023", detail: "Three Ways Leaders Should\n" +
+      id: 7, icon: <SlCalender/>, date: "5th January,2023", detail: "777Three Ways Leaders Should\n" +
         "Pursue Breakthrough Innovation\n" +
         "During...", img: `${news2}`
     },
     {
-      id: 8, icon: <SlCalender/>, date: "5th January,2023", detail: "Three Ways Leaders Should\n" +
+      id: 8, icon: <SlCalender/>, date: "5th January,2023", detail: "888Three Ways Leaders Should\n" +
         "Pursue Breakthrough Innovation\n" +
         "During...", img: `${news2}`
     }
         ]
 
-let box=document.querySelector(".cards");
-
-  console.log(box.scrollLeft,"boxx")
-
-  const slideWidth = 30;
-  const length = newsList.length;
-  console.log(newsList.length,"uparrrrr")
-  // newsList.push(...newsList);
+  const [currentImage, setCurrentImage] = useState(0);
 
 
-  const sleep = (ms = 0) => {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  };
-
-
-
-  const createItem = (position, idx) => {
-    const item = {
-      styles: {
-        transform: `translateX(${position * slideWidth}rem)`,
-      },
-      player: newsList[idx],
-    };
-
-    // switch (position) {
-    //   case length - 1:
-    //   case length + 1:
-    //     item.styles = {...item.styles, filter: 'grayscale(1)'};
-    //     break;
-    //   case length:
-    //     break;
-    //   default:
-    //     item.styles = {...item.styles, opacity: 0};
-    //     break;
-    // }
-
-      return item;
-  };
-
-
-
-
-  const CarouselSlideItem = ({pos, idx, activeIdx}) => {
-    const item = createItem(pos, idx, activeIdx);
-
-
-    return (
-      <li className="carousel__slide-item" style={item.styles}>
-        <card className="card">
-              <p className="card-date">{item.player.icon}{item.player.date}</p>
-              <p className="card-detail">{item.player.detail}</p>
-              <img className="card-img" src={item.player.img}/>
-        </card>
-      </li>
-
-
-    );
-  };
-
-
-  const keys = Array.from(Array(newsList.length).keys());
-  console.log(Array.from(Array(newsList.length)),"arraylist")
-  // const [currentNews,setCurrentNews] =useState(newsCarousel)
-  const [items, setItems] = useState(keys);
-  const [isTicking, setIsTicking] =useState(false);
-  const [activeIdx, setActiveIdx] = useState(0);
-  const bigLength = items.length;
-
-
-  const handleLeft=(jump = 1)=>{
-    let widthOne=box.clientWidth;
-    box.scrollLeft=box.scrollLeft - widthOne
-
-    // if (!isTicking) {
-    //   setIsTicking(true);
-    //   setItems((prev) => {
-    //     return prev.map((_, i) => prev[(i + jump) % bigLength]);
-    //   });
-    // }
-
-
+  const handleLeft=()=>{
+    setCurrentImage((currentImage - 1 + newsList.length) % newsList.length);
+    console.log(currentImage);
+    
   }
-  const handleRight=(jump = 1)=>{
-    let widthOne=box.clientWidth;
-    console.log(widthOne,"widthone")
-    box.scrollLeft=box.scrollLeft + widthOne
-    console.log(box.scrollLeft - widthOne,"scrollLeft")
-    // if (!isTicking) {
-    //   setIsTicking(true);
-    //   setItems((prev) => {
-    //     return prev.map(
-    //       (_, i) => prev[(i - jump + bigLength) % bigLength],
-    //     );
-    //   });
-    // }
+
+
+  const handleRight=()=>{
+    console.log(newsList.length,"newlistttt");
+    setCurrentImage((currentImage + 1) % newsList.length);
+    console.log(currentImage,"right");
 
   }
 
-  useEffect(() => {
-    if (isTicking) sleep(300).then(() => setIsTicking(false));
-  }, [isTicking]);
-
-
-
+  
 
   return (
     <div className="news">
@@ -162,13 +76,24 @@ let box=document.querySelector(".cards");
 
       <div className="news-bottom">
         <div className="news-card">
-          <button className="vector-left" src={vectorLeft} onClick={handleLeft}><p>&lt;</p></button>
-          <button className="vector-right" src={vectorRight} onClick={handleRight}> <p>&gt;</p></button>
+          <button className="vector-left" src={vectorLeft} onClick={handleLeft}>&lt;</button>
+          <button className="vector-right" src={vectorRight} onClick={handleRight}> &gt;</button>
 
           <div className="cards">
 
 
-            {newsList.map( (n ) => {
+          {newsList.slice(currentImage, currentImage + 3).map((image, index) => (
+
+          <card className="card">
+                  <p className="card-date">{image.icon}{image.date}</p>
+                  <p className="card-detail">{image.detail}</p>
+                  <img className="card-img" src={image.img}/>
+                </card>
+          ))
+
+
+
+            /* {newsList.map( (n ) => {
               return (
                 <card className="card">
                   <p className="card-date">{n.icon}{n.date}</p>
@@ -176,15 +101,9 @@ let box=document.querySelector(".cards");
                   <img className="card-img" src={n.img}/>
                 </card>
                )
-              // return(
-              // <CarouselSlideItem
-              //   key={i}
-              //   idx={i}
-              //   pos={pos}
-              //   activeIdx={activeIdx}
-              // />
+            
 
-            } )}
+            } )} */}
 
           </div>
 
